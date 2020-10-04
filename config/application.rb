7270476fa0ccf44b3493c654e11130229948ae09
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-10-04 19:34:53 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,28 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SearchLaundry
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework :rspec,
+      controller_specs: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
