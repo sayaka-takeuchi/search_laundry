704af52f1bf28c970a552980341a0a2558b23f5c
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-10-05 09:24:16 +0900

    モデル作成

---
+++ app/models/laundry.rb
@@ -0,0 +1,15 @@
+class Laundry < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+  with_options presence: true do
+    validates :name
+    validates :address
+    validates :opening_date
+    validates :open_time
+    validates :close_time
+    validates :show_washing
+    validates :futon_washing
+    validates :dryer
+    validates :washing_machine
+  end
+end
