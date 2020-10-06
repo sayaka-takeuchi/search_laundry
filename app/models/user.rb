78915a2ed81193144173a580319b900678197ea0
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-10-06 14:34:03 +0900

    バリデーション指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :comments
+  validates :admin, inclusion: {in: [true,false]}
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  NAME_REGEX = /\A[あ-んァ-ン一-龥]+\z/
+  NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  with_options presence: true do
+    validates :nickname
+
+    with_options format: {with: NAME_REGEX, message: 'には全角日本語を使用してください'} do
+      validates :first_name
+      validates :last_name
+    end
+    
+    with_options format: {with: NAME_KANA_REGEX, message: 'には全角カタカナを使用してください'} do
+    validates :last_name_kana
+    validates :first_name_kana
+    end
+  end
 end
