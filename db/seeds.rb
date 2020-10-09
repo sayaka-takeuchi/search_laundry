bf3e25754353b8b76f8b3f13586969f9e35c108e
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-10-09 15:37:48 +0900

    seedファイル変更(画像付与)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,25 @@
-Laundry.create(name: 'コインランドリー豊橋店', address: '豊橋市1-1',opening_date: '2012-1-13', open_time: '7', close_time: '23', shoe_washing: true, futon_washing: true, washing_machine: true )
-Laundry.create(name: 'コインランドリー豊橋東店', address: '豊橋市東1-2',opening_date: '2014-3-13', open_time: '6', close_time: '23', washing_machine: true )
-Laundry.create(name: 'コインランドリー豊橋みゆき店', address: '豊橋市南5-2',opening_date: '2014-4-28', open_time: '5', close_time: '23', shoe_washing: true, washing_machine: true )
-Laundry.create(name: 'コインランドリー豊橋朝日店', address: '豊橋市1-5-3',opening_date: '2014-9-7', open_time: '7', close_time: '22', shoe_washing: true, washing_machine: true )
-Laundry.create(name: 'コインランドリー豊川店', address: '豊川市1-48',opening_date: '2015-6-28', open_time: '6', close_time: '23', futon_washing: true, washing_machine: true )
-Laundry.create(name: 'コインランドリー豊橋向山店', address: '豊橋市向山町452',opening_date: '2016-12-19', open_time: '7', close_time: '23', washing_machine: true )
-Laundry.create(name: 'コインランドリー名古屋西店', address: '名古屋市西区40',opening_date: '2017-1-13', open_time: '6', close_time: '24', shoe_washing: true, dryer: true )
-Laundry.create(name: 'コインランドリー名古屋南通店', address: '名古屋市南区63-9',opening_date: '2018-5-28', open_time: '7', close_time: '23', shoe_washing: true, dryer: true, washing_machine: true )
-Laundry.create(name: 'コインランドリー岡崎店', address: '岡崎市45-83',opening_date: '2019-12-22', open_time: '7', close_time: '23', shoe_washing: true, dryer: true, washing_machine: true )
-Laundry.create(name: 'コインランドリー岡崎東店', address: '岡崎市東町463',opening_date: '2020-6-8', open_time: '6', close_time: '22', futon_washing: true, dryer: false, washing_machine: true )
+20.times do |n|
+  laundry = Laundry.new(
+    name: "コインランドリー#{n + 1}号店",
+    address: "愛知県名古屋市1-#{n + 1}",
+    opening_date: Faker::Date.between(from: '2000-01-01', to: Date.today),
+    open_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    close_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
+    shoe_washing: %w(true false).sample,
+    futon_washing: %w(true false).sample,
+    dryer: %w(true false).sample,
+    washing_machine: %w(true false).sample,
+    ) 
+  laundry.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+  laundry.save
+end
+User.create(
+  nickname: 'admin', 
+  first_name: '鈴木', 
+  last_name: '太郎', 
+  first_name_kana: 'スズキ',
+  last_name_kana: 'タロウ', 
+  email: 'test@test.com',
+  password: 'aaaa1111',
+  admin: true
+)
