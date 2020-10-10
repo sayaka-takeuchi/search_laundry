dcc223639cb900ab100f09b1a057434e177f4137
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-10-10 18:01:08 +0900

    Commentモデル　テスト実行

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+  it 'コメントの値があれば保存されること' do
+    expect(@comment).to be_valid
+  end
+
+  it 'コメントが空の場合保存がされないこと' do
+    @comment.text = nil
+    @comment.valid?
+    expect(@comment.errors.full_messages).to include('コメントを入力してください')
+  end
 end
