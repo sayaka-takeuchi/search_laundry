4c59d0ae113735877a68f23ff876c4760c929b14
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-10-11 20:08:51 +0900

    seedファイル変更(csvファイルに変更)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
-20.times do |n|
+require "csv"
+
+CSV.foreach('db/store.csv', headers: true) do |row|
   laundry = Laundry.new(
-    name: "コインランドリー#{n + 1}号店",
-    address: "愛知県名古屋市1-#{n + 1}",
-    opening_date: Faker::Date.between(from: '2000-01-01', to: Date.today),
-    open_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-    close_time: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now),
-    shoe_washing: %w(true false).sample,
-    futon_washing: %w(true false).sample,
-    dryer: %w(true false).sample,
-    washing_machine: %w(true false).sample,
-    ) 
+    name: row['name'] ,
+    address: row['address'],
+    opening_date: row['opening_date'],
+    open_time: row['open_time'],
+    close_time: row['close_time'],
+    shoe_washing: row['shoe_washing'],
+    futon_washing: row['futon_washing'],
+    dryer: row['dryer'],
+    washing_machine: row['washing_machine']
+  )
   laundry.image.attach(io: File.open('public/images/image.jpg'), filename: 'test_image.jpg')
   laundry.save
 end
