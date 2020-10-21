495b282a957de5f3b0c5d5420ed9b867b924ff46
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-10-21 20:59:41 +0900

    Commentモデル　テストコード追加

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :comment do
     text { Faker::Lorem.sentence }
+    gender { 1 }
+    age_id { 1 }
+    rate_id { 1 } 
     association :user
     association :laundry
   end
