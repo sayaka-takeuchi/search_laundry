d2bbc025c7ac0ce68e27c1aab3d9d443555f5a1b
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-11-17 09:34:00 +0900

    バグ修正(#14)

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,19 +1,28 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  # ゲストユーザーとしてログイン
   def new_guest
-    user = User.find_or_create_by!(nickname: 'ゲストユーザー', email: 'user@example.com') do |admin|
-      admin.password = SecureRandom.alphanumeric
+    user = User.find_or_create_by!(nickname: 'ゲストユーザー', email: 'user@example.com') do |user|
+      user.password = make_password
     end
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 
+  # ゲスト管理者としてログイン
   def new_admin
-    user = User.find_or_create_by!(nickname: 'ゲスト管理者', email: 'admin@example.com', admin: true) do |admin|
-      admin.password = SecureRandom.alphanumeric
+    admin = User.find_or_create_by!(nickname: 'ゲスト管理者', email: 'admin@example.com', admin: true) do |admin|
+      admin.password = make_password
     end
-    sign_in user
+    sign_in admin
     redirect_to root_path, notice: 'ゲスト管理者としてログインしました。'
+  end
+
+  private
+
+  # ランダムでパスワードを作成(英数字が必ず入る)
+  def make_password
+    SecureRandom.alphanumeric(10) + [*'a'..'z'].sample(1).join + [*'0'..'9'].sample(1).join
   end
 end
