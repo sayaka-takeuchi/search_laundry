3ddbfde2fb23d8036434f5bac619761482de6aa3
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-11-20 14:21:03 +0900

    不要jsファイル削除

--- app/javascript/user_nav.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  document.getElementById('dropdown-toggle').onclick = () => {
-    const dropdownMenu = document.getElementById('dropdown-menu');
-    dropdownMenu.classList.toggle('active');
-  }
-}
-  document.addEventListener('turbolinks:load', function(){
-    toggleMenu();
-  });
