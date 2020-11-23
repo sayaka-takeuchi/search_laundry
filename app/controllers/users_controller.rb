323f9d59fde4ba499435e85490a33b3b70be3ea1
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-11-23 13:10:22 +0900

    他ユーザーのマイページを閲覧不可にする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
+  before_action :check_user, only: [:show]
 
   def show
     @comments = @user.comments.includes(:laundry)
   end
 
   private
+
+  def check_user
+    redirect_to root_path unless user_signed_in? && current_user.id == @user.id
+  end
 
   def set_user
     @user = User.find(params[:id])
