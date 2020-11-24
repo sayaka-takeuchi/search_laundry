46196615c02fd89c7be8ca86f7d9268da07817fb
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-11-24 09:51:33 +0900

    request specリファクタリング

--- spec/requests/laundries_spec.rb
+++ spec/requests/laundries_spec.rb
@@ -10,36 +10,29 @@ RSpec.describe 'Laundries', type: :request do
   end
 
   describe '#show' do
-    before do
-      @laundry = FactoryBot.create(:laundry)
-    end
+    let(:laundry) { FactoryBot.create(:laundry) }
 
     it '正常にレスポンスを返すこと' do
-      get laundry_path(@laundry)
+      get laundry_path(laundry)
       expect(response).to be_successful
     end
   end
 
   describe '#new' do
-    context '管理者の場合' do
-      before do
-        @user = FactoryBot.create(:user, admin: true)
-      end
+    let(:admin_user) { FactoryBot.create(:user, admin: true) }
+    let(:user) { FactoryBot.create(:user, admin: false) }
 
+    context '管理者の場合' do
       it '正常にレスポンスを返すこと' do
-        sign_in @user
+        sign_in admin_user
         get new_laundry_path
         expect(response).to be_successful
       end
     end
 
     context '認可されていないユーザーの場合' do
-      before do
-        @user = FactoryBot.create(:user, admin: false)
-      end
-
       it 'トップページにリダイレクトすること' do
-        sign_in @user
+        sign_in user
         get new_laundry_path
         expect(response).to redirect_to root_path
       end
@@ -60,14 +53,12 @@ RSpec.describe 'Laundries', type: :request do
 
   describe '#create' do
     context '管理者の場合' do
-      before do
-        @user = FactoryBot.create(:user, admin: true)
-      end
+      let(:admin_user) { FactoryBot.create(:user, admin: true) }
 
       context "有効な属性値の場合" do
         it '店舗情報を追加できること' do
           laundry_params = FactoryBot.attributes_for(:laundry, image: fixture_file_upload('/files/test_image.png'))
-          sign_in @user
+          sign_in admin_user
           expect {
             post laundries_path, params: { laundry: laundry_params }
           }.to change { Laundry.count }.by(1)
@@ -77,7 +68,7 @@ RSpec.describe 'Laundries', type: :request do
       context "無効な属性値の場合" do
         it "店舗情報を登録できないこと" do
           laundry_params = FactoryBot.attributes_for(:laundry, :invalid)
-          sign_in @user
+          sign_in admin_user
           expect {
             post laundries_path, params: { laundry: laundry_params }
           }.to_not change { Laundry.count }
@@ -86,13 +77,11 @@ RSpec.describe 'Laundries', type: :request do
     end
 
     context '認可されていないユーザーの場合' do
-      before do
-        @user = FactoryBot.create(:user, admin: false)
-      end
+      let(:user) { FactoryBot.create(:user, admin: false) }
 
       it '店舗情報を追加できないこと' do
         laundry_params = FactoryBot.attributes_for(:laundry, image: fixture_file_upload('/files/test_image.png'))
-        sign_in @user
+        sign_in user
         expect {
           post laundries_path, params: { laundry: laundry_params }
       }.to_not change { Laundry.count }
@@ -100,7 +89,7 @@ RSpec.describe 'Laundries', type: :request do
 
       it 'トップページにリダイレクトすること' do
         laundry_params = FactoryBot.attributes_for(:laundry, image: fixture_file_upload('/files/test_image.png'))
-        sign_in @user
+        sign_in user
         post laundries_path, params: { laundry: laundry_params }
         expect(response).to redirect_to root_path
       end
@@ -116,104 +105,83 @@ RSpec.describe 'Laundries', type: :request do
   end
 
   describe '#edit' do
-    context '管理者の場合' do
-      before do
-        @user = FactoryBot.create(:user, admin: true)
-        @laundry = FactoryBot.create(:laundry)
-      end
+    let(:admin_user) { FactoryBot.create(:user, admin: true) }
+    let(:user) { FactoryBot.create(:user, admin: false) }
+    let!(:laundry) { FactoryBot.create(:laundry) }
 
+    context '管理者の場合' do
       it '正常にレスポンスを返すこと' do
-        sign_in @user
-        get edit_laundry_path(@laundry)
+        sign_in admin_user
+        get edit_laundry_path(laundry)
         expect(response).to be_successful
       end
     end
 
     context '認可されていないユーザーの場合' do
-      before do
-        @user = FactoryBot.create(:user, admin: false)
-        @laundry = FactoryBot.create(:laundry)
-      end
-
       it 'トップページにリダイレクトすること' do
-          sign_in @user
-          get edit_laundry_path(@laundry)
+          sign_in user
+          get edit_laundry_path(laundry)
           expect(response).to redirect_to root_path
       end
     end
 
     context 'ゲストユーザーの場合' do
-      before do
-        @laundry = FactoryBot.create(:laundry)
-      end
-
       it '302レスポンスを返すこと' do
-        get edit_laundry_path(@laundry)
+        get edit_laundry_path(laundry)
         expect(response).to have_http_status '302'
       end
 
       it 'トップページにリダイレクトすること' do
-        get edit_laundry_path(@laundry)
+        get edit_laundry_path(laundry)
         expect(response).to redirect_to root_path
       end
     end
   end
 
   describe '#update' do
-    context '管理者の場合' do
-      before do
-        @user = FactoryBot.create(:user, admin: true)
-        @laundry = FactoryBot.create(:laundry, name: 'コインランドリー名古屋店')
-      end
+    let(:admin_user) { FactoryBot.create(:user, admin: true) }
+    let(:user) { FactoryBot.create(:user, admin: false) }
+    let(:laundry) { FactoryBot.create(:laundry, name: 'コインランドリー名古屋店') }
 
+    context '管理者の場合' do
       context "有効な属性値の場合" do
         it '店舗情報を更新できること' do
           laundry_params = FactoryBot.attributes_for(:laundry, 
             name: 'コインランドリー岡崎店', 
             image: fixture_file_upload('/files/test_image.png'))
-          sign_in @user
-          patch laundry_path(@laundry), params: { laundry: laundry_params }
-          expect(@laundry.reload.name).to eq 'コインランドリー岡崎店'
+          sign_in admin_user
+          patch laundry_path(laundry), params: { laundry: laundry_params }
+          expect(laundry.reload.name).to eq 'コインランドリー岡崎店'
         end
       end
       
       context '無効な属性値の場合' do
-        it '店舗情報を更新できないこと'
+        it '店舗情報を更新できないこと' do
           laundry_params = FactoryBot.attributes_for(:laundry, :invalid)
-          sign_in @user
-          expect {
-            patch laundry_path(@laundry), params: { laundry: :invalid }
-          }.to_not change { Laundry.count }
+          sign_in admin_user
+          patch laundry_path(laundry), params: { laundry: laundry_params }
+          expect(laundry.reload.name).to eq 'コインランドリー名古屋店'
         end
       end
     end
 
     context '認可されていないユーザーの場合' do
-      before do
-        @user = FactoryBot.create(:user, admin: false)
-        @laundry = FactoryBot.create(:laundry, name: 'コインランドリー名古屋店')
-      end
-
       it '店舗情報を更新できないこと' do
         laundry_params = FactoryBot.attributes_for(:laundry, 
           name: 'コインランドリー岡崎店', 
           image: fixture_file_upload('/files/test_image.png'))
-        sign_in @user
-        patch laundry_path(@laundry), params: { laundry: laundry_params }
-        expect(@laundry.reload.name).to eq 'コインランドリー名古屋店'
+        sign_in user
+        patch laundry_path(laundry), params: { laundry: laundry_params }
+        expect(laundry.reload.name).to eq 'コインランドリー名古屋店'
       end
     end
 
     context 'ゲスト(未ログインユーザー)の場合' do
-      before do
-        @laundry = FactoryBot.create(:laundry, name: 'コインランドリー名古屋店')
-      end
-
       it '302レスポンスを返すこと' do
         laundry_params = FactoryBot.attributes_for(:laundry, 
           name: 'コインランドリー岡崎店', 
           image: fixture_file_upload('/files/test_image.png'))
-        patch laundry_path(@laundry), params: { laundry: laundry_params }
+        patch laundry_path(laundry), params: { laundry: laundry_params }
         expect(response).to have_http_status '302'
       end
 
@@ -221,65 +189,55 @@ RSpec.describe 'Laundries', type: :request do
         laundry_params = FactoryBot.attributes_for(:laundry, 
           name: 'コインランドリー岡崎店', 
           image: fixture_file_upload('/files/test_image.png'))
-        patch laundry_path(@laundry), params: { laundry: laundry_params }
+        patch laundry_path(laundry), params: { laundry: laundry_params }
         expect(response).to redirect_to root_path
       end
     end
   end
 
   describe '#destroy' do
-    context '管理者の場合' do
-      before do
-        @user = FactoryBot.create(:user, admin: true)
-        @laundry = FactoryBot.create(:laundry)
-      end
+    let(:admin_user) { FactoryBot.create(:user, admin: true) }
+    let(:user) { FactoryBot.create(:user, admin: false) }
+    let!(:laundry) { FactoryBot.create(:laundry) }
 
+    context '管理者の場合' do
       it '店舗情報を削除できること' do
-        sign_in @user
+        sign_in admin_user
         expect {
-          delete laundry_path(@laundry)
+          delete laundry_path(laundry)
       }.to change { Laundry.count }.by(-1)
       end
     end
 
     context '認可されていないユーザーの場合' do
-      before do
-        @user = FactoryBot.create(:user, admin: false)
-        @laundry = FactoryBot.create(:laundry)
-      end
-
       it '店舗情報を削除できないこと' do
-        sign_in @user
+        sign_in user
         expect {
-          delete laundry_path(@laundry)
+          delete laundry_path(laundry)
         }.to_not change { Laundry.count }
       end
 
       it 'トップページにリダイレクトすること' do
-        sign_in @user
-        delete laundry_path(@laundry)
+        sign_in user
+        delete laundry_path(laundry)
         expect(response).to redirect_to root_path
       end
     end
 
     context 'ゲスト(未ログインユーザー)の場合' do
-      before do
-        @laundry = FactoryBot.create(:laundry)
-      end
-
       it '302レスポンスをかえすこと' do
-        delete laundry_path(@laundry)
+        delete laundry_path(laundry)
         expect(response).to have_http_status '302'
       end
 
       it 'トップページにリダイレクトすること' do
-        delete laundry_path(@laundry)
+        delete laundry_path(laundry)
         expect(response).to redirect_to root_path
       end
 
       it '店舗情報を削除できないこと' do
         expect {
-          delete laundry_path(@laundry)
+          delete laundry_path(laundry)
         }.to_not change { Laundry.count }
       end
     end
